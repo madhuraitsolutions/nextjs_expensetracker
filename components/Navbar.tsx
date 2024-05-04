f8a4e454fa3c66ce9d01641aff6da1466ffeaf5c
Kombiah <kombiahrk@yahoo.com>
2024-05-04 16:51:21 +0530

    Crated NavbarItem() function and include it in DesktopNavbar() function and imported necessary things

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,11 @@
+"use client"
+
 import React from 'react'
 import Logo from './Logo'
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { buttonVariants } from './ui/button';
 
 function Navbar() {
   return (
@@ -21,10 +27,40 @@ function DesktopNavbar() {
       <nav className='container flex items-center justify-between px-8'>
         <div className='flex h-[80px] min-h-[60px] items-center gap-x-4'>
           <Logo />
+          <div className='flex h-full'>
+            {items.map((item) => (
+              <NavbarItem
+                key={item.label}
+                link={item.link}
+                label={item.label}
+              />
+            ))}
+          </div>
         </div>
       </nav>
     </div>
   )
+}
+
+function NavbarItem({ link, label }: { link: string; label: string }) {
+    const pathname = usePathname();
+    const isActive = pathname === link;
+
+    return (
+      <div className='relative flex items-center'>
+        <Link 
+          href={link}
+          className={cn(buttonVariants({variant:"ghost"}),
+            "w-full justify-start text-lg text-muted-foreground hover:text-foreground", isActive && "text-foreground"
+          )}
+        >
+          {label}
+        </Link>
+        {isActive && (
+          <div className='absolute -bottom-[2px] left-1/2 hidden h-[2px] w-[80%] -translate-x-1/2 rounded-xl bg-foreground md:block'></div>
+        )}
+      </div>
+    )
 }
 
 export default Navbar
