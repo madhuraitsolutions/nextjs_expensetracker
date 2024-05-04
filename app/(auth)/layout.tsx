5c27d20148b78554a9313491f458ecd5a9231d85
Kombiah <kombiahrk@yahoo.com>
2024-05-04 13:07:04 +0530

    Update the layout.tsx className to center the content to the page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,8 +2,8 @@ import React, { ReactNode } from 'react'
 
 function layout({children}:{children:ReactNode}) {
   return (
-    <div className=''>
-        <div className=''>{children}</div>
+    <div className='relative h-screen w-full flex flex-col items-center justify-center'>
+        <div className='mt-12'>{children}</div>
     </div>
   )
 }
