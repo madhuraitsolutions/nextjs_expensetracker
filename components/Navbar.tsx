1749abf6fbc1007a9abb8ae19187d72f67fa5d16
Kombiah <kombiahrk@yahoo.com>
2024-05-04 16:22:09 +0530

    create items as array object type to store navigation label, link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,12 +9,18 @@ function Navbar() {
   )
 }
 
+const items = [
+  { label: "Dashboard", link: "/" },
+  { label: "Transactions", link: "/transactions" },
+  { label: "Manage", link: "/manage" }
+];
+
 function DesktopNavbar() {
   return (
     <div className='hidden border-separate border-b bg-background md:block'>
       <nav className='container flex items-center justify-between px-8'>
         <div className='flex h-[80px] min-h-[60px] items-center gap-x-4'>
-          <Logo/>
+          <Logo />
         </div>
       </nav>
     </div>
