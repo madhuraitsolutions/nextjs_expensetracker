fed2c18779ff2cbd2fda06ef370f852b11a3c5a5
Kombiah <kombiahrk@yahoo.com>
2024-05-04 13:36:43 +0530

    Create Logo.tsx under components folder and update the code

---
+++ components/Logo.tsx
@@ -0,0 +1,13 @@
+import { HandCoins } from 'lucide-react'
+import React from 'react'
+
+function Logo() {
+    return (
+        <a href="/" className='flex gap-2'>
+            <HandCoins className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
+            <p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter  text-transparent'>BudgetTracker</p>
+        </a>
+    )
+}
+
+export default Logo
