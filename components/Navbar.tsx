e0d7fbd346326c3931d1014e42f95c686f367e89
Kombiah <kombiahrk@yahoo.com>
2024-05-04 16:12:01 +0530

    Import and include the Logo component in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Logo from './Logo'
 
 function Navbar() {
   return (
@@ -10,7 +11,13 @@ function Navbar() {
 
 function DesktopNavbar() {
   return (
-    <div>DesktopNavbar</div>
+    <div className='hidden border-separate border-b bg-background md:block'>
+      <nav className='container flex items-center justify-between px-8'>
+        <div className='flex h-[80px] min-h-[60px] items-center gap-x-4'>
+          <Logo/>
+        </div>
+      </nav>
+    </div>
   )
 }
 
