7b96b6a2ad59a708c5577885aa6a48fc1c0e29fe
Kombiah <kombiahrk@yahoo.com>
2024-05-07 15:13:46 +0530

    Update the page.tsx of dashboard folder

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -23,9 +23,10 @@ async function page() {
 
   return (
     <div className='h-full bg-background'>
-      <div className='border-b bg-card'>
-        <div className='container flex flex-wrap items-center justify-between'>
+      <div className='border-b bg-gray-800'>
+        <div className='container flex flex-wrap items-center justify-between gap-6 py-8'>
           <p className='text-3xl font-bold'>Hello, {user.firstName}! </p>
+
         </div>
       </div>
     </div>
