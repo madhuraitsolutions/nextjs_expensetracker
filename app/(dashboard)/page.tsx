c7640d47ac84e397b72df59f32974daad5fa8242
Kombiah <kombiahrk@yahoo.com>
2024-05-07 15:31:40 +0530

    update the page.tsx of dashboard folder to add New Income and New Expense buttons below p tag

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import prisma from '@/lib/prisma';
 import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation';
@@ -23,10 +24,23 @@ async function page() {
 
   return (
     <div className='h-full bg-background'>
-      <div className='border-b bg-gray-800'>
+      <div className='border-b bg-card'>
         <div className='container flex flex-wrap items-center justify-between gap-6 py-8'>
           <p className='text-3xl font-bold'>Hello, {user.firstName}! </p>
-
+          <div className='flex items-center gap-3'>
+            <Button
+              variant={"outline"}
+              className='border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white'
+            >
+              New Income
+            </Button>
+            <Button
+              variant={"outline"}
+              className='border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white'
+            >
+              New Expense
+            </Button>
+          </div>
         </div>
       </div>
     </div>
