c03d0d5f2efab14484e5593a6226554726cedea5
Kombiah <kombiahrk@yahoo.com>
2024-05-08 00:17:44 +0530

    update the CategoryPicker component to fetch the categories from api using useQuery

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { TransactionType } from '@/lib/types';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 
 interface Props {
-  type:TransactionType
+  type: TransactionType
 }
 
 function CategoryPicker({ type }: Props) {
+  const categoriesQuery = useQuery({
+    queryKey: ["categories", type],
+    queryFn: () => fetch(`/api/categories?type=${type}`)
+      .then((res) => res.json()),
+  })
   return (
     <div>CategoryPicker</div>
   )
