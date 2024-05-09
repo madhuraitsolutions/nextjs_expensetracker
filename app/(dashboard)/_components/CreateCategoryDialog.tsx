5cb15ead6ccf5dbebc2ea2c2084c6fb2e98cd335
Kombiah <kombiahrk@yahoo.com>
2024-05-09 14:35:04 +0530

    Add the Icon field to the CreateCategoryDialog Component below the Name field

--- app/(dashboard)/_components/CreateCategoryDialog.tsx
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -4,13 +4,16 @@ import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Form, FormField, FormItem, FormLabel, FormControl, FormDescription } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { TransactionType } from '@/lib/types';
 import { cn } from '@/lib/utils';
 import { CreateCategorySchema, CreateCategorySchemaType } from '@/schema/category';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { PlusSquare } from 'lucide-react';
+import { CircleOff, PlusSquare } from 'lucide-react';
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
+import Picker from '@emoji-mart/react';
+import data from '@emoji-mart/data';
 
 interface Props {
     type: TransactionType
@@ -63,6 +66,46 @@ function CreateCategoryDialog({ type }: Props) {
                                     </FormControl>
                                     <FormDescription>
                                         Category Name
+                                    </FormDescription>
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="icon"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Icon</FormLabel>
+                                    <FormControl>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <Button variant={"outline"} className='h-[100px] w-full'>
+                                                    {form.watch("icon") ? (
+                                                        <div className='flex flex-col items-center gap-2'>
+                                                            <span className='text-5xl' role='img'>{field.value}</span>
+                                                            <p className='text-xs text-muted-foreground'>Click to change</p>
+                                                        </div>
+                                                    ) : (
+                                                        <div className='flex flex-col items-center gap-2'>
+                                                            <CircleOff className='h-[48px] w-[48px]' />
+                                                            <p className='text-xs text-muted-foreground'>Click to select</p>
+                                                        </div>
+                                                    )}
+                                                </Button>
+                                            </PopoverTrigger>
+                                            <PopoverContent className='w-full'>
+                                                <Picker
+                                                    data={data}
+                                                    onEmojiSelect={(emoji: { native: string }) => {
+                                                        field.onChange(emoji.native);
+                                                    }}
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is how you category will appear in the app
                                     </FormDescription>
                                 </FormItem>
                             )}
