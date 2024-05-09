92a67f4709e249b298c42061f7ca40a358f00b19
Kombiah <kombiahrk@yahoo.com>
2024-05-09 12:03:16 +0530

    Create CreateCategoryDialog.tsx Component under the components folder of dashboard and update with code

---
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { TransactionType } from '@/lib/types';
+import { cn } from '@/lib/utils';
+import { CreateTransactionSchema, CreateTransactionSchemaType } from '@/schema/transaction';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { PlusSquare } from 'lucide-react';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+
+interface Props {
+    type: TransactionType
+}
+
+function CreateCategoryDialog({ type }: Props) {
+    const [open, setOpen] = useState(false);
+
+    const form = useForm<CreateTransactionSchemaType>({
+        resolver : zodResolver(CreateTransactionSchema),
+        defaultValues : {
+            type,
+        }
+    });
+    
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <Button
+                    variant={"ghost"}
+                    className='flex border-seprate items-center justify-start rounded-none border-b px-3 py-3 text-muted-foreground'
+                >
+                    <PlusSquare className='mr-2 h-4 w-4'/>
+                    Create New
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Create 
+                        <span className={cn("mx-1", type === "income" ? "text-emerald-500" : "text-rose-500")}>
+                            {type}
+                        </span>
+                        category
+                    </DialogTitle>
+                    <DialogDescription>
+                        Categories are used to group your transactions
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default CreateCategoryDialog
