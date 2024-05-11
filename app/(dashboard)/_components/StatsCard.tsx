ab6341c8c5040121b5d42a43d17c569d44697d13
Kombiah <kombiahrk@yahoo.com>
2024-05-11 12:44:13 +0530

    minor update

--- app/(dashboard)/_components/StatsCard.tsx
+++ app/(dashboard)/_components/StatsCard.tsx
@@ -92,7 +92,7 @@ function StatCard({ formatter, title, value, icon, }: {
     return (
         <Card className='flex h-24 w-full items-center gap-2 p-4'>            
             {icon}
-            <div className='flex flex-col gap-0'>
+            <div className='flex flex-col items-start gap-0'>
                 <p className='text-muted-background'>{title}</p>
                 <CountUp
                     preserveValue
