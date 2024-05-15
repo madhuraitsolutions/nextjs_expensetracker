640b1fda113c6084695b772098d302d776ec6d24
Kombiah <kombiahrk@yahoo.com>
2024-05-15 15:43:37 +0530

    Update CreateCategoryDialog component to accept the trigger as optional props

--- app/(dashboard)/_components/CreateCategoryDialog.tsx
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -10,7 +10,7 @@ import { cn } from '@/lib/utils';
 import { CreateCategorySchema, CreateCategorySchemaType } from '@/schema/category';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { CircleOff, Loader2, PlusSquare } from 'lucide-react';
-import React, { useCallback, useState } from 'react'
+import React, { ReactNode, useCallback, useState } from 'react'
 import { useForm } from 'react-hook-form';
 import Picker from '@emoji-mart/react';
 import data from '@emoji-mart/data';
@@ -23,9 +23,10 @@ import { useTheme } from 'next-themes';
 interface Props {
     type: TransactionType;
     successCallback: (category: Category) => void;
+    trigger?: ReactNode
 }
 
-function CreateCategoryDialog({ type, successCallback }: Props) {
+function CreateCategoryDialog({ type, successCallback, trigger }: Props) {
     const [open, setOpen] = useState(false);
 
     const form = useForm<CreateCategorySchemaType>({
@@ -76,13 +77,17 @@ function CreateCategoryDialog({ type, successCallback }: Props) {
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
-                <Button
-                    variant={"ghost"}
-                    className='flex border-seprate items-center justify-start rounded-none border-b px-3 py-3 text-muted-foreground'
-                >
-                    <PlusSquare className='mr-2 h-4 w-4' />
-                    Create New
-                </Button>
+                {trigger ? (
+                    trigger
+                ) : (
+                    <Button
+                        variant={"ghost"}
+                        className='flex border-seprate items-center justify-start rounded-none border-b px-3 py-3 text-muted-foreground'
+                    >
+                        <PlusSquare className='mr-2 h-4 w-4' />
+                        Create New
+                    </Button>
+                )}
             </DialogTrigger>
             <DialogContent>
                 <DialogHeader>
