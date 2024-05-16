9f578ef90d6b41d9a97b379b851e17c4943cf84a
Kombiah <kombiahrk@yahoo.com>
2024-05-16 14:42:43 +0530

    Import the TransactionsTable component in the TransactionsPage and fragment the code

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -5,6 +5,7 @@ import { MAX_DATE_RANGE_DAYS } from '@/lib/constants';
 import { differenceInDays, startOfMonth } from 'date-fns';
 import React, { useState } from 'react'
 import { toast } from 'sonner';
+import TransactionsTable from './_components/TransactionsTable';
 
 function TranasactionsPage() {
     const [dateRange, SetDateRange] = useState<{ from: Date, to: Date }>({
@@ -13,29 +14,34 @@ function TranasactionsPage() {
     });
 
     return (
-        <div className='border-b bg-card'>
-            <div className='container flex flex-wrap items-center justify-between gap-6 py-8'>
-                <div>
-                    <p className='text-3xl font-bold'>Tranasaction History</p>
+        <>
+            <div className='border-b bg-card'>
+                <div className='container flex flex-wrap items-center justify-between gap-6 py-8'>
+                    <div>
+                        <p className='text-3xl font-bold'>Tranasaction History</p>
+                    </div>
+                    <DateRangePicker
+                        initialDateFrom={dateRange.from}
+                        initialDateTo={dateRange.to}
+                        showCompare={false}
+                        onUpdate={values => {
+                            const { from, to } = values.range;
+
+                            if (!from || !to) return;
+                            if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+                                toast.error("Max date range is " + MAX_DATE_RANGE_DAYS + " days");
+                                return;
+                            }
+
+                            SetDateRange({ from, to });
+                        }}
+                    />
                 </div>
-                <DateRangePicker
-                    initialDateFrom={dateRange.from}
-                    initialDateTo={dateRange.to}
-                    showCompare={false}
-                    onUpdate={values => {
-                        const { from, to } = values.range;
-
-                        if (!from || !to) return;
-                        if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
-                            toast.error("Max date range is " + MAX_DATE_RANGE_DAYS + " days");
-                            return;
-                        }
-
-                        SetDateRange({ from, to });
-                    }}
-                />
             </div>
-        </div>
+            <div className='container'>
+                <TransactionsTable from={dateRange.from} to={dateRange.to} />
+            </div>
+        </>
     )
 }
 
