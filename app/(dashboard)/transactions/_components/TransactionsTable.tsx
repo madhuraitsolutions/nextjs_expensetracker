3676339f75b8fb440565e0e45995b3f660fa5338
Kombiah <kombiahrk@yahoo.com>
2024-05-16 16:10:28 +0530

    Update the TransactionsTable.tsx Component
    fetch the data from transactions-history api
    Define columns
    Initializing the Table Instance
    Define Table

--- app/(dashboard)/transactions/_components/TransactionsTable.tsx
+++ app/(dashboard)/transactions/_components/TransactionsTable.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { GetTransactionsHistoryResponseType } from '@/app/api/transactions-history/route';
+import SkeletonWrapper from '@/components/SkeletonWrapper';
+import { Table, TableBody, TableCell, TableHead, TableRow, TableHeader } from '@/components/ui/table';
+import { useQuery } from '@tanstack/react-query';
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
 import React from 'react'
 
 interface Props {
@@ -7,9 +12,88 @@ interface Props {
     to: Date;
 }
 
+const emptyData: any[] = [];
+
+type TransactionsHistoryRow = GetTransactionsHistoryResponseType[0];
+
+export const columns: ColumnDef<TransactionsHistoryRow>[] = [
+    {
+        accessorKey: "category",
+        header: "Category",
+        cell: ({ row }) => (
+            <div className='flex gap-2 capitalize'>
+                {row.original.categoryIcon}
+                <div className='capitalize'>{row.original.category}</div>
+            </div>
+        ),
+    },
+]
+
 function TransactionsTable({ from, to }: Props) {
+
+    const history = useQuery<GetTransactionsHistoryResponseType>({
+        queryKey: ["transaction", "history", from, to],
+        queryFn: () => fetch(`/api/transactions-history?from=${from}&to=${to}`).then((res) => res.json()),
+    });
+
+    const table = useReactTable({
+        data: history.data || emptyData,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    });
+
     return (
-        <div>TransactionsTable</div>
+        <div className='w-full'>
+            <div className='flex flex-wrap items-end justify-between gap-2 py-4'>
+                Todo: filters
+            </div>
+            <SkeletonWrapper isLoading={history.isFetching}>
+                <div className="rounded-md border">
+                    <Table>
+                        <TableHeader>
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <TableRow key={headerGroup.id}>
+                                    {headerGroup.headers.map((header) => {
+                                        return (
+                                            <TableHead key={header.id}>
+                                                {header.isPlaceholder
+                                                    ? null
+                                                    : flexRender(
+                                                        header.column.columnDef.header,
+                                                        header.getContext()
+                                                    )}
+                                            </TableHead>
+                                        )
+                                    })}
+                                </TableRow>
+                            ))}
+                        </TableHeader>
+                        <TableBody>
+                            {table.getRowModel().rows?.length ? (
+                                table.getRowModel().rows.map((row) => (
+                                    <TableRow
+                                        key={row.id}
+                                        data-state={row.getIsSelected() && "selected"}
+                                    >
+                                        {row.getVisibleCells().map((cell) => (
+                                            <TableCell key={cell.id}>
+                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))
+                            ) : (
+                                <TableRow>
+                                    <TableCell colSpan={columns.length} className="h-24 text-center">
+                                        No results.
+                                    </TableCell>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </div>
+            </SkeletonWrapper>
+        </div>
     )
 }
 
