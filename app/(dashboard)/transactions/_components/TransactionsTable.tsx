cfc6b241fbbd014a65e88b37c76a499834e679b5
Kombiah <kombiahrk@yahoo.com>
2024-05-16 14:33:34 +0530

    Create the TransactionsTable.tsx component in app\(dashboard)\transactions\_components\ to display the transactions details in table format

---
+++ app/(dashboard)/transactions/_components/TransactionsTable.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from 'react'
+
+interface Props {
+    from: Date;
+    to: Date;
+}
+
+function TransactionsTable({ from, to }: Props) {
+    return (
+        <div>TransactionsTable</div>
+    )
+}
+
+export default TransactionsTable
